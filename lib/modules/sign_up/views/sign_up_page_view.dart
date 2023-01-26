6318e83d5ae850e6e7a20c370debc6f69d9f72fe
Sai Karthik <saikarthiksistla@gmail.com>
2023-01-26 21:13:03 +0530

    textfields hint text added for sign up page

--- lib/modules/sign_up/views/sign_up_page_view.dart
+++ lib/modules/sign_up/views/sign_up_page_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:mms_assignment/common/values/app_colors.dart';
+import 'package:mms_assignment/common/widgets/buttons.dart';
 import 'package:mms_assignment/common/widgets/textfield.dart';
 
 class SignUpPageView extends StatelessWidget {
@@ -17,61 +19,76 @@ class SignUpPageView extends StatelessWidget {
       body: SafeArea(
         child: Container(
           padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            children: [
-              RichText(
-                text: const TextSpan(
-                  text: "Global",
-                  style: TextStyle(
-                      color: AppColors.primaryColor,
-                      fontSize: 36.0,
-                      fontWeight: FontWeight.w900),
-                  children: [
-                    TextSpan(
-                      text: 'FINANCE',
-                      style: TextStyle(
-                          color: AppColors.textColor,
-                          fontSize: 36.0,
-                          fontWeight: FontWeight.w300),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 180.0,
+                  child: Center(
+                    child: RichText(
+                      text: const TextSpan(
+                        text: "Global",
+                        style: TextStyle(
+                            color: AppColors.primaryColor,
+                            fontSize: 36.0,
+                            fontWeight: FontWeight.w900),
+                        children: [
+                          TextSpan(
+                            text: 'FINANCE',
+                            style: TextStyle(
+                                color: AppColors.textColor,
+                                fontSize: 36.0,
+                                fontWeight: FontWeight.w300),
+                          ),
+                        ],
+                      ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
-              CustomTextField(
-                hintText: "hint",
-                onChanged: (val) {},
-              ),
-              const SizedBox(height: 16.0),
-              CustomTextField(
-                hintText: "hint",
-                onChanged: (val) {},
-              ),
-              const SizedBox(height: 16.0),
-              CustomTextField(
-                hintText: "hint",
-                onChanged: (val) {},
-              ),
-              const SizedBox(height: 16.0),
-              CustomTextField(
-                hintText: "hint",
-                onChanged: (val) {},
-              ),
-              const SizedBox(height: 16.0),
-              CustomTextField(
-                hintText: "hint",
-                onChanged: (val) {},
-              ),
-              const SizedBox(height: 16.0),
-              CustomTextField(
-                hintText: "hint",
-                onChanged: (val) {},
-              ),
-              const SizedBox(height: 16.0),
-              CustomTextField(
-                hintText: "hint",
-                onChanged: (val) {},
-              ),
-            ],
+                CustomTextField(
+                  hintText: "First Name",
+                  onChanged: (val) {},
+                ),
+                const SizedBox(height: 16.0),
+                CustomTextField(
+                  hintText: "Middle Name",
+                  onChanged: (val) {},
+                ),
+                const SizedBox(height: 16.0),
+                CustomTextField(
+                  hintText: "Last Name",
+                  onChanged: (val) {},
+                ),
+                const SizedBox(height: 16.0),
+                CustomTextField(
+                  hintText: "Need Date of Birth Widget",
+                  onChanged: (val) {},
+                ),
+                const SizedBox(height: 16.0),
+                CustomTextField(
+                  hintText: "Confirm Password",
+                  onChanged: (val) {},
+                ),
+                const SizedBox(height: 16.0),
+                CustomTextField(
+                  hintText: "Phone Number",
+                  onChanged: (val) {},
+                ),
+                const SizedBox(height: 16.0),
+                CustomTextField(
+                  hintText: "Need Gender Dropdown",
+                  onChanged: (val) {},
+                ),
+                const SizedBox(height: 32.0),
+                PrimaryButton(buttonText: "Create Account", onTap: () {}),
+                const SizedBox(height: 20.0),
+                const Text(
+                  "By tapping \"Create Account\",You will\n accept all Terms & Conditions",
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 40.0),
+              ],
+            ),
           ),
         ),
       ),
