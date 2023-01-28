69f279ed0a0e1d321adbbfdb56d3c63ae1b5bffb
Sai Karthik <saikarthiksistla@gmail.com>
2023-01-28 19:07:32 +0530

    launcher added for ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:mms_assignment/common/values/strings.dart';
 import 'package:mms_assignment/initial_bindings.dart';
